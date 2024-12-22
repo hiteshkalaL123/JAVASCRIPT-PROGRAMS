5ab452015421dd87bf120b74770ea43f96c2bcfb
hiteshkalaL123 <167711834+hiteshkalaL123@users.noreply.github.com>
2024-12-23 01:36:37 +0530

    changes made

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ let num1=10;
 let num2=20;
 let result=num1+num2;
 console.log(result);
+const AccountID=12345
+let accountName="hitesh"
+let accountState="mumbai"
+let accountbranch;
+console.table([AccountID,accountName,accountState,accountbranch]);
