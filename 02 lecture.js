e35191c7e20d649e3d133074273acbf11db991af
hiteshkalaL123 <167711834+hiteshkalaL123@users.noreply.github.com>
2024-12-23 19:05:13 +0530

    conversion of datatypes

---
+++ 02 lecture.js
@@ -0,0 +1,26 @@
+
+//we give the value null then  output is 0 this is drawback
+//let score=null
+//we give the value undefined then result is NaN ->
+//let score = undefined
+//let score ="30"
+//let score="30abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+//we convert the number into string and srting into number using
+// string(variable we declare )
+
+/*let valueinNumber = Number(score)
+console.log(typeof  valueinNumber);
+console.log(valueinNumber);*/
+
+
+let booleanIs= "hitesh"
+ let Isboolean = Boolean(booleanIs)
+ console.log(Isboolean );
+
+  //1=>true;
+  //0=>false
+  //empty agar diye to result me false ayega
+
+ 
